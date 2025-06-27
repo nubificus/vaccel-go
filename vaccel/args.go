11b58e612668e570b1ca937919f705fe224d14ea
Ilias Lagomatis <ilago@nubificus.co.uk>
2025-06-27 14:14:35 +0000

    feat: Upgrade args API
     - chore: Rename source code file
     - feat: Introduce `AddStringArg` and `AddInt32Arg` for easier argument handling
     - fix: Update argument type in Arg-API functions
    
    Signed-off-by: Ilias Lagomatis <ilago@nubificus.co.uk>

--- vaccel/args.go
+++ vaccel/args.go
@@ -37,30 +37,56 @@ func ArgsInit(size uint32) *ArgList {
 	return list
 }
 
-func (arglist *ArgList) AddSerialArg(buf unsafe.Pointer, size uintptr) int {
+func (arglist *ArgList) AddSerialArg(buf unsafe.Pointer, size int) int {
 	return int(C.vaccel_add_serial_arg(arglist.cList, buf, C.uint(size)))
+}
+
+func (arglist *ArgList) AddStringArg(arg string) int {
+	cStr := C.CString(arg)
+	ret := int(C.vaccel_add_serial_arg(arglist.cList, unsafe.Pointer(cStr), C.uint(C.strlen(cStr))))
+	if ret == OK {
+		length := int(arglist.cList.size)
+		idx := int(arglist.cList.curr_idx - 1)
+		slice := (*[1 << 16]C.int)(unsafe.Pointer(arglist.cList.idcs_allocated_space))[:length:length]
+		slice[idx] = 1
+	} else {
+		C.free(unsafe.Pointer(cStr))
+	}
+	return ret
+}
+
+func (arglist *ArgList) AddInt32Arg(arg int32) int {
+	cInt := (*C.int32_t)(C.malloc(C.sizeof_int32_t))
+	*cInt = C.int32_t(arg)
+	ret := int(C.vaccel_add_serial_arg(arglist.cList, unsafe.Pointer(cInt), C.sizeof_int32_t))
+	if ret == OK {
+		length := int(arglist.cList.size)
+		idx := int(arglist.cList.curr_idx - 1)
+		slice := (*[1 << 16]C.int)(unsafe.Pointer(arglist.cList.idcs_allocated_space))[:length:length]
+		slice[idx] = 1
+	} else {
+		C.free(unsafe.Pointer(cInt))
+	}
+	return ret
 }
 
 func (arglist *ArgList) AddNonSerialArg(nonSerialBuf unsafe.Pointer,
 	argtype uint32, serialize Serializer) int { //nolint:revive // argtype will be used in a next iteration
-
 	serialBuf, bytes := serialize(nonSerialBuf)
-
-	return arglist.AddSerialArg(serialBuf, uintptr(bytes))
+	return arglist.AddSerialArg(serialBuf, int(bytes))
 }
 
-func (arglist *ArgList) ExpectSerialArg(buf unsafe.Pointer, size uintptr) int {
+func (arglist *ArgList) ExpectSerialArg(buf unsafe.Pointer, size int) int {
 	return int(C.vaccel_expect_serial_arg(arglist.cList, buf, C.uint(size)))
 }
 
-func (arglist *ArgList) ExpectNonSerialArg(expectedSize uintptr) int {
+func (arglist *ArgList) ExpectNonSerialArg(expectedSize int) int {
 	return int(C.vaccel_expect_nonserial_arg(arglist.cList, C.uint(expectedSize)))
 }
 
 func (arglist *ArgList) GetArgs() *Arg {
 	args := new(Arg)
 	args.cArg = arglist.cList.list
-
 	return args
 }
 
